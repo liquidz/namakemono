60edc3cc28dab05a0d6473292266c678b322be92
liquidz <liquidz.uo@gmail.com>
2009-01-21 19:16:49 +0900

    update setup file

--- setup.sh
+++ setup.sh
@@ -3,6 +3,14 @@
 pwd > .nmk_setup_tmp
 dir=`sed -e 's/\//\\\\\//g' .nmk_setup_tmp`
 
+if [ -e "namakemono.scm" ]; then
+	rm -f namakemono.scm
+fi
+
 sed -e "s/@NMK_PATH@/$dir/g" namakemono.scm.base > namakemono.scm
+sed -e "s/@NMK_PATH@/$dir/g" nmk.base > nmk
+
+chmod 444 namakemono.scm
+chmod 755 nmk
 
 rm -f .nmk_setup_tmp
